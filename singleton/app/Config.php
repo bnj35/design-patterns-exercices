940a0f8d9327d1df643b8545ae6f01039f1a9305
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2025-03-10 16:42:37 +0100

    feat: :sparkles: Implement Singleton pattern with Config class and update Docker configuration

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,27 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+class Config
+{
+    private static $instance = null;
+    private $settings;
+
+    private function __construct()
+    {
+        $this->settings = require __DIR__ . '/../config/config.php';
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function get($key)
+    {
+        return $this->settings[$key] ?? null;
+    }
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -5,3 +5,15 @@ require('../vendor/autoload.php');
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+
+use App\Config;
+
+// Récuperer une instance de Config
+$config = Config::getInstance();
+
+// Afficher une valeur contenu dans config.php
+echo $config->get('apiKey');
+
+// Récupérer une seconde instance de Config et vérifier que les deux instances sont identiques
+$config2 = Config::getInstance();
+var_dump($config === $config2); // true
